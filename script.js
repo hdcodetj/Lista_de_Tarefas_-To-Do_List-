917de98ab8bee77ddc0bedc1f8e1511db2ea29b3
Hélio <heliodctj@gmail.com>
2025-02-12 20:59:05 -0300

    Adicionando interação com JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const inputTarefa = document.getElementById('inputTarefa');
 const btnAdicionar = document.getElementById('btnAdicionar');
 const listaTarefas = document.getElementById('listaTarefas');
+
+
+function adicionarTarefa() {
+    const textoTarefa = inputTarefa.value.trim();
+
+    if (textoTarefa === '') {
+        alert('Digite uma tarefa antes de adicionar!');
+        return;
+    }
+    const novaTarefa = document.createElement('li');
+    novaTarefa.textContent = textoTarefa;
+
+    listaTarefas.appendChild(novaTarefa);
+
+    inputTarefa.value = '';
+}
+btnAdicionar.addEventListener('click', adicionarTarefa);
